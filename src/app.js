daca40090b4699827218245f77d8127490192ddd
ishigaki <ishigaki@cayenne.co.jp>
2018-01-03 01:37:05 +0900

    初期コミット

---
+++ src/app.js
@@ -0,0 +1,10 @@
+var html = {
+  index: require('./html/index.html')
+};
+
+var css = {
+  fontAwesome: require('font-awesome/scss/font-awesome.scss'),
+  style: require('./scss/style.scss')
+};
+require('./js/common.js');
+
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = [{
+  entry: ['./src/app.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src', 'html')
+  },
+  module: {
+    rules: [{
+      test: /\.(html?)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: './[name].[ext]'
+        }
+      }
+    }, {
+      test: /\.(css|sass|scss)$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            plugins: function () {
+              return [
+                require('autoprefixer')
+              ];
+            }
+          }
+        }
+      ]
+    }, {
+      test: /\.(jpe?g|png|gif|svg|ico)(\?.+)?$/,
+      include: [
+        path.resolve(__dirname, 'src', 'img')
+      ],
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          name: './img/[name].[ext]'
+        }
+      }
+    }, {
+      test: /\.(eot|otf|ttf|woff2?|svg)(\?.+)?$/,
+      include: [
+        path.resolve(__dirname, 'node_modules')
+      ],
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: './fonts/[name].[ext]'
+        }
+      }
+    }]
+  },
+  plugins: [
+    new webpack.ProvidePlugin(
+      {
+        jQuery: "jquery",
+        $: "jquery",
+      }
+    ),
+  ],
+}];
