27b5c06dcf653e24f89f995394631b8368785d2f
ishigaki <ishigaki@cayenne.co.jp>
2018-01-03 23:47:01 +0900

    【CSS】normalize.cssを追加

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ var html = {
 };
 
 var css = {
+  normalize: require('normalize.css/normalize.css'),
   fontAwesome: require('font-awesome/scss/font-awesome.scss'),
   style: require('./scss/style.scss')
 };
